1a727c904aee0d062d5cce6a406e5336d3a7b80b
SensitiveTeeth <SensitiveTeeth@users.noreply.github.com>
2022-07-15 07:47:38 +0000

    deploy: 1ac4c717a28240df7feb7bb4514a6c54f5dd0c21

---
+++ precache-manifest.f4ca3e45bdbcea670a658801550654e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f45eeccbfd5fdf6f5bdc5b921e8fda7",
+    "url": "https://SensitiveTeeth.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "218059c1299398953d9a",
+    "url": "https://SensitiveTeeth.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "2a579763aed4154c4ef6",
+    "url": "https://SensitiveTeeth.github.io/task-management-frontend/static/js/2.be382b08.chunk.js"
+  },
+  {
+    "revision": "218059c1299398953d9a",
+    "url": "https://SensitiveTeeth.github.io/task-management-frontend/static/js/main.cf4bdc25.chunk.js"
+  },
+  {
+    "revision": "b52484f9abd6973d58f3",
+    "url": "https://SensitiveTeeth.github.io/task-management-frontend/static/js/runtime~main.c80bb3c4.js"
+  }
+]);
